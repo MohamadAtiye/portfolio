1eeaf99591eee9d5c5db14383b105f5093fbadd8
Mohamad Atiye <mohamad.atiye@gmail.com>
2024-05-26 19:10:03 +0300

    make static class for webgl, add controls

---
+++ todo-list/src/components/DropDownPopper.tsx
@@ -0,0 +1,53 @@
+import { Box, IconButton, Paper, Popover } from "@mui/material";
+import React, { ReactNode, useState } from "react";
+
+interface DropDownPopperProps {
+  children: ReactNode;
+  ButtonIcon: ReactNode;
+}
+export default function DropDownPopper({
+  children,
+  ButtonIcon,
+}: DropDownPopperProps) {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
+
+  return (
+    <div>
+      <IconButton
+        aria-controls="burger-menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+        color="inherit"
+      >
+        {ButtonIcon}
+      </IconButton>
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+      >
+        <Paper sx={{ zIndex: 9999 }}>
+          <Box p={2} width={200}>
+            {children}
+          </Box>
+        </Paper>
+      </Popover>
+    </div>
+  );
+}
--- todo-list/src/components/GLBackground.tsx
+++ todo-list/src/components/GLBackground.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useRef } from "react";
-
-// src/webgl-utils.ts
+import { Box, Slider, Typography } from "@mui/material";
+import { useEffect, useRef, useState } from "react";
+import DropDownPopper from "./DropDownPopper";
+import SettingsIcon from "@mui/icons-material/Settings";
 
 function createShader(
   gl: WebGLRenderingContext,
@@ -57,6 +58,7 @@ const fragmentShaderSource = `
   uniform vec2 u_resolution;
   uniform vec3 u_color;
   uniform float u_blobSize;
+  uniform vec3 u_bgColor;
 
   #define NUM_BLOBS 6
 
@@ -66,6 +68,18 @@ const fragmentShaderSource = `
     st.x *= u_resolution.x / u_resolution.y;
 
     vec2 blobs[NUM_BLOBS];
+    // for (int i = 0; i < NUM_BLOBS; i++) {
+    //   // float dist = length(st - blobs[i]);
+    //   // value += u_blobSize / dist;
+
+    //   float step = 2.0 / float(NUM_BLOBS);
+    //   float istep = -1.0 +  float(i) * step;
+    //   // vec2 center =  vec2(-1.0 + (step * float(i)) , 0.0);
+    //   vec2 center =  vec2(istep , 0.0);
+    //   float rotateRadius = 0.3;
+
+    //   blobs[i] = center + vec2(sin(u_time), cos(u_time)) * rotateRadius;
+    // }
     blobs[0] = vec2(0.5, 0.5) + vec2(sin(u_time), cos(u_time)) * 0.2;
     blobs[1] = vec2(-0.5, -0.5) + vec2(sin(u_time * 1.2), cos(u_time * 1.2)) * 0.3;
     blobs[2] = vec2(0.3, -0.5) + vec2(sin(u_time * 0.8), cos(u_time * 0.8)) * 0.25;
@@ -84,28 +98,71 @@ const fragmentShaderSource = `
     
     vec3 color = u_color * value;
 
-    // Set the background color to light blue
-    vec3 backgroundColor = vec3(0.6, 0.8, 1.0);
     // Combine the background color with the blob value
-    vec3 finalColor = u_color * value + backgroundColor;
+    vec3 finalColor = u_color * value + u_bgColor;
 
     // render final color
     gl_FragColor = vec4(finalColor, 1.0);
   }
 `;
 
-export default function GLBackground() {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
+const hexToRgb = (hex: string): [number, number, number] => {
+  // Remove the hash symbol if it exists
+  const trimmedHex = hex.replace(/^#/, "");
 
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    if (!canvas) return;
+  // Parse r, g, b values
+  const bigint = parseInt(trimmedHex, 16);
+  const r = (bigint >> 16) & 255;
+  const g = (bigint >> 8) & 255;
+  const b = bigint & 255;
 
+  return [r, g, b];
+};
+function normRgbToHex([r, g, b]: number[]) {
+  // Convert normalized RGB values to 0-255 range
+  r = Math.round(r * 255);
+  g = Math.round(g * 255);
+  b = Math.round(b * 255);
+
+  // Convert each component to hexadecimal and concatenate them
+  const hex = ((r << 16) | (g << 8) | b).toString(16).padStart(6, "0");
+
+  // Prepend '#' to the hexadecimal value
+  return `#${hex}`;
+}
+
+class Renderer {
+  static canvas: HTMLCanvasElement;
+  static gl: WebGLRenderingContext;
+  static program: WebGLProgram;
+
+  // attributes
+  static positionBuffer: WebGLBuffer | null;
+  static positionAttributeLocation: number;
+
+  //draw options (uniform)
+  static uniformLocations = {
+    u_resolution: null as WebGLUniformLocation | null,
+    u_time: null as WebGLUniformLocation | null,
+    u_color: null as WebGLUniformLocation | null,
+    u_blobSize: null as WebGLUniformLocation | null,
+    u_bgColor: null as WebGLUniformLocation | null,
+  };
+
+  static uniformValues = {
+    uv_bgColor: [0.6, 0.8, 1.0],
+    uv_blobSize: 0.1,
+  };
+
+  static init(canvas: HTMLCanvasElement) {
     const gl = canvas.getContext("webgl");
     if (!gl) {
       console.error("WebGL not supported");
       return;
     }
+
+    Renderer.canvas = canvas;
+    Renderer.gl = gl;
 
     const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
     const fragmentShader = createShader(
@@ -118,77 +175,172 @@ export default function GLBackground() {
     const program = createProgram(gl, vertexShader, fragmentShader);
     if (!program) return;
 
-    const positionAttributeLocation = gl.getAttribLocation(
-      program,
+    Renderer.program = program;
+
+    Renderer.resize();
+
+    Renderer.start();
+  }
+
+  static resize() {
+    Renderer.canvas.width = window.innerWidth;
+    Renderer.canvas.height = window.innerHeight;
+    if (Renderer.gl) {
+      Renderer.gl.viewport(0, 0, Renderer.canvas.width, Renderer.canvas.height);
+    }
+  }
+
+  static start() {
+    if (!Renderer.gl || !Renderer.program) return;
+    const gl = Renderer.gl; //just a shorter ref
+
+    Renderer.positionAttributeLocation = gl.getAttribLocation(
+      Renderer.program,
       "a_position"
     );
-    const resolutionUniformLocation = gl.getUniformLocation(
-      program,
-      "u_resolution"
-    );
-    const timeUniformLocation = gl.getUniformLocation(program, "u_time");
-    const colorUniformLocation = gl.getUniformLocation(program, "u_color");
-    const blobSizeUniformLocation = gl.getUniformLocation(
-      program,
-      "u_blobSize"
-    );
 
-    const positionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    // define unform variable locations (variables per full render)
+    Renderer.uniformLocations = {
+      u_resolution: gl.getUniformLocation(Renderer.program, "u_resolution"),
+      u_time: gl.getUniformLocation(Renderer.program, "u_time"),
+      u_color: gl.getUniformLocation(Renderer.program, "u_color"),
+      u_blobSize: gl.getUniformLocation(Renderer.program, "u_blobSize"),
+      u_bgColor: gl.getUniformLocation(Renderer.program, "u_bgColor"),
+    };
+
+    Renderer.positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, Renderer.positionBuffer);
     const positions = new Float32Array([
       -1, -1, 1, -1, -1, 1, -1, 1, 1, -1, 1, 1,
     ]);
     gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
 
-    const render = (time: number) => {
-      time *= 0.001; // change the movement speed here
+    requestAnimationFrame(Renderer.render);
+  }
 
-      gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+  static render(time: number) {
+    if (!Renderer.gl || !Renderer.program) return;
+    const gl = Renderer.gl; //just a shorter ref
 
-      gl.clear(gl.COLOR_BUFFER_BIT);
+    time *= 0.001; // change the movement speed here
 
-      gl.useProgram(program);
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
-      gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-      gl.enableVertexAttribArray(positionAttributeLocation);
-      gl.vertexAttribPointer(
-        positionAttributeLocation,
-        2,
-        gl.FLOAT,
-        false,
-        0,
-        0
-      );
+    gl.clear(gl.COLOR_BUFFER_BIT);
 
-      gl.uniform2f(
-        resolutionUniformLocation,
-        gl.canvas.width,
-        gl.canvas.height
-      );
-      gl.uniform1f(timeUniformLocation, time);
-      gl.uniform3f(colorUniformLocation, -0.4, -0.6, -0.5); // 0.8, 0.2, 0.2 Change the color here (R, G, B)
-      gl.uniform1f(blobSizeUniformLocation, 0.1); // 0.3 Change the size of the blobs here
+    gl.useProgram(Renderer.program);
 
-      gl.drawArrays(gl.TRIANGLES, 0, 6);
+    // pass attributes (per vertext variables)
+    gl.bindBuffer(gl.ARRAY_BUFFER, Renderer.positionBuffer);
+    gl.enableVertexAttribArray(Renderer.positionAttributeLocation);
+    gl.vertexAttribPointer(
+      Renderer.positionAttributeLocation,
+      2,
+      gl.FLOAT,
+      false,
+      0,
+      0
+    );
 
-      requestAnimationFrame(render);
-    };
+    const { u_resolution, u_time, u_color, u_blobSize, u_bgColor } =
+      Renderer.uniformLocations;
+    const { uv_bgColor, uv_blobSize } = Renderer.uniformValues;
 
-    requestAnimationFrame(render);
+    // pass uniform (full render variables)
+    gl.uniform2f(u_resolution, gl.canvas.width, gl.canvas.height);
+    gl.uniform1f(u_time, time);
+    gl.uniform3f(u_color, -0.4, -0.6, -0.5); // 0.8, 0.2, 0.2 Change the color here (R, G, B)
+    gl.uniform1f(u_blobSize, uv_blobSize);
+    gl.uniform3f(u_bgColor, uv_bgColor[0], uv_bgColor[1], uv_bgColor[2]);
+
+    gl.drawArrays(gl.TRIANGLES, 0, 6);
+
+    //repeat
+    requestAnimationFrame(Renderer.render);
+  }
+}
+
+export default function GLBackground() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  // prepare GL and program
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    Renderer.init(canvasRef.current);
   }, []);
 
+  // size and resize handler
+  useEffect(() => {
+    function resizeCanvas() {
+      if (Renderer.canvas) {
+        Renderer.resize();
+      }
+    }
+
+    resizeCanvas();
+    window.addEventListener("resize", resizeCanvas);
+
+    return () => {
+      window.removeEventListener("resize", resizeCanvas);
+    };
+  }, []);
+
+  const [blobSize, setBlobSize] = useState<number>(
+    Renderer.uniformValues.uv_blobSize
+  );
+  const [color, setColor] = useState<string>(
+    normRgbToHex(Renderer.uniformValues.uv_bgColor)
+  );
+
+  const handleSizeChange = (_event: Event, newValue: number | number[]) => {
+    if (!Array.isArray(newValue)) {
+      setBlobSize(newValue);
+      Renderer.uniformValues.uv_blobSize = Number(newValue);
+    }
+  };
+
+  const handleColorChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setColor(event.target.value);
+
+    const normalized = hexToRgb(event.target.value).map((value) => value / 255);
+    Renderer.uniformValues.uv_bgColor = normalized;
+  };
+
   return (
-    <canvas
-      style={{
-        height: "100%",
-        width: "100%",
-        position: "absolute",
-        top: 0,
-        left: 0,
-        background: "black",
-        zIndex: -1,
-      }}
-      ref={canvasRef}
-    />
+    <>
+      <Box sx={{ position: "absolute", top: 0, right: 0, padding: 1 }}>
+        <DropDownPopper ButtonIcon={<SettingsIcon />}>
+          <Typography variant="caption" gutterBottom>
+            Blob Size {blobSize}
+          </Typography>
+          <Slider
+            value={blobSize}
+            onChange={handleSizeChange}
+            valueLabelDisplay="auto"
+            min={0.01}
+            max={0.2}
+            step={0.01}
+          />
+          <Typography variant="caption" gutterBottom>
+            BgColor {color}
+          </Typography>
+          <input type="color" value={color} onChange={handleColorChange} />
+        </DropDownPopper>
+      </Box>
+      <canvas
+        style={{
+          height: "100%",
+          width: "100%",
+          position: "absolute",
+          top: 0,
+          left: 0,
+          background: "black",
+          zIndex: -1,
+        }}
+        ref={canvasRef}
+      />
+    </>
   );
 }
