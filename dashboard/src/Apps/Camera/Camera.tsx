4590009225c8cea29171961b1a5e826fd59f0533
Mohamad Atiye <mohamad.atiye@gmail.com>
2024-06-26 16:40:57 +0300

    added video effects: blur

--- dashboard/src/Apps/Camera/Camera.tsx
+++ dashboard/src/Apps/Camera/Camera.tsx
@@ -1,13 +1,16 @@
-import { Box, Select, MenuItem, Typography } from "@mui/material";
+import { Box, Select, MenuItem, Typography, Button } from "@mui/material";
 import { useEffect, useRef, useState } from "react";
 
 export default function Camera() {
   const stream = useRef<MediaStream | null>(null);
+  // const secondStream = useRef<MediaStream | null>(null);
+
   const videoRef = useRef<HTMLVideoElement | null>(null);
   const [devices, setDevices] = useState<MediaDeviceInfo[]>([]);
   const [selectedDeviceId, setSelectedDeviceId] = useState<string>("");
   const [selectedResolution, setSelectedResolution] =
     useState<string>("640x480");
+  const [isBlur, setIsBlur] = useState(false);
 
   const [videoSettings, setVideoSettings] = useState<{
     width: number;
@@ -58,7 +61,6 @@ export default function Camera() {
   // handle creating stream on device change
   useEffect(() => {
     if (selectedDeviceId && !stream.current) {
-      console.log("eff1");
       const [width, height] = "640x480".split("x").map(Number);
 
       navigator.mediaDevices
@@ -81,7 +83,6 @@ export default function Camera() {
           });
 
           const capabilities = track.getCapabilities();
-          console.log(capabilities);
           setVideoCapabilities({
             width: Number(capabilities.width?.max) || width,
             height: Number(capabilities.height?.max) || height,
@@ -103,10 +104,9 @@ export default function Camera() {
     }
   }, [selectedDeviceId]);
 
+  // handle resolution change
   useEffect(() => {
     if (stream.current) {
-      console.log("eff2");
-
       const [width, height] = selectedResolution.split("x").map(Number);
       const track = stream.current.getVideoTracks()[0];
       track
@@ -122,7 +122,6 @@ export default function Camera() {
           });
 
           const capabilities = track.getCapabilities();
-          console.log(capabilities);
           setVideoCapabilities({
             width: Number(capabilities.width?.max) || width,
             height: Number(capabilities.height?.max) || height,
@@ -130,6 +129,80 @@ export default function Camera() {
         });
     }
   }, [selectedResolution]);
+
+  useEffect(() => {
+    let trackProcessor: MediaStreamTrackProcessor<VideoFrame>;
+    let trackGenerator: MediaStreamVideoTrackGenerator;
+    if (stream.current) {
+      if (!isBlur) {
+        if (videoRef.current) videoRef.current.srcObject = stream.current;
+        return;
+      }
+
+      const videoTrack = stream.current.getVideoTracks()[0];
+
+      // create proccessor object that will handle frame changes
+      const trackProcessor = new MediaStreamTrackProcessor({
+        track: videoTrack,
+      });
+      // create generator which composes frames back into a stream
+      const trackGenerator = new MediaStreamTrackGenerator({ kind: "video" });
+
+      const canvas = new OffscreenCanvas(640, 360);
+      const context = canvas.getContext("2d", { willReadFrequently: true })!;
+
+      const transformer = new TransformStream({
+        async transform(
+          videoFrame: VideoFrame,
+          controller: TransformStreamDefaultController
+        ) {
+          canvas.width = videoFrame.displayWidth;
+          canvas.height = videoFrame.displayHeight;
+
+          // Draw the frame onto the canvas
+          context.drawImage(videoFrame, 0, 0);
+
+          // Apply a blur filter
+          context.filter = "blur(10px)";
+          context.drawImage(canvas, 0, 0);
+
+          // Create a new video frame from the canvas
+          const blurredFrame = new VideoFrame(canvas, {
+            timestamp: videoFrame.timestamp,
+          });
+
+          // Enqueue the blurred frame
+          controller.enqueue(blurredFrame);
+
+          // Close the original frame to free up resources
+          videoFrame.close();
+        },
+      });
+
+      trackProcessor.readable
+        .pipeThrough(transformer)
+        .pipeTo(trackGenerator.writable);
+
+      const streamAfter = new MediaStream([trackGenerator]);
+
+      if (videoRef.current) {
+        videoRef.current.srcObject = streamAfter;
+      }
+    }
+
+    return () => {
+      try {
+        // Close the generator
+        if (trackGenerator) trackGenerator.stop();
+
+        // Close the processor readable and generator writable streams
+        if (trackProcessor) trackProcessor.readable.cancel();
+        if (trackGenerator) trackGenerator.writable.abort();
+      } catch (e) {
+        console.error(e);
+      }
+    };
+  }, [isBlur]);
 
   return (
     <Box
@@ -223,6 +296,10 @@ export default function Camera() {
             </MenuItem>
           ))}
         </Select>
+
+        <Button onClick={() => setIsBlur((p) => !p)}>
+          {isBlur ? "unblur" : "blur"}
+        </Button>
       </Box>
     </Box>
   );
