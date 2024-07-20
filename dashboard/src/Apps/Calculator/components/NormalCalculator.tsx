9f80ffb044e00e99722e092d7cd53c701710a5e9
Mohamad Atiye <mohamad.atiye@gmail.com>
2024-07-20 14:07:56 +0300

    functioning calculator

--- dashboard/src/Apps/Calculator/components/NormalCalculator.tsx
+++ dashboard/src/Apps/Calculator/components/NormalCalculator.tsx
@@ -1,12 +1,142 @@
-import { Box, Button, Grid, TextField } from "@mui/material";
-import { useState } from "react";
+import {
+  Box,
+  Button,
+  Grid,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useRef, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+function countSpecificCharacter(str: string, charToCount: string) {
+  let count = 0;
+  for (const char of str) {
+    if (char === charToCount) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+function isNumber(val: string) {
+  return !isNaN(Number(val) + 1);
+}
+
+function getLastFullNumber(val: string): number {
+  // Use a regular expression to match all numbers in the string
+  const matches = val.match(/-?\d+(\.\d+)?/g);
+
+  // Check if there are any matches and return the last one as a number
+  if (matches && matches.length > 0) {
+    return parseFloat(matches[matches.length - 1]);
+  }
+
+  // If no matches found, return NaN or throw an error
+  return NaN;
+}
+
+const handleInput = (old: string, value: string): string => {
+  const oc = countSpecificCharacter(old, "(");
+  const cc = countSpecificCharacter(old, ")");
+  const lastChar = old.charAt(old.length - 1);
+
+  console.log({ oc, cc, lastChar });
+
+  // handle parenthesis cases
+  if (value === "()") {
+    //handle opening normal
+    if (["=", "-", "*", "/", "("].indexOf(lastChar) > -1 || old.length === 0) {
+      return old + "(";
+    }
+
+    // handle opening with multiplication after number or closing
+    if (oc === cc && (isNumber(lastChar) || lastChar === ")")) {
+      return old + "*(";
+    }
+
+    // handle after dot
+    if (lastChar === ".") {
+      return old + "0" + (oc > cc ? ")" : "*(");
+    }
+
+    // handle closing
+    if (oc > cc) return old + (lastChar === "." ? "0" : "") + ")";
+  }
+
+  if (value === ".") {
+    // handle empty
+    if (old.length === 0) return "0.";
+
+    // handle after operation of opening
+    if (["=", "-", "*", "/", "("].indexOf(lastChar) > -1) {
+      return old + "0.";
+    }
+
+    // handle after closing
+    if (lastChar === ")") return old + "*0.";
+
+    // handle no multi dot per number
+    // point after number
+    if (isNumber(lastChar) && getLastFullNumber(old) % 1 === 0) {
+      return old + ".";
+    }
+  }
+
+  // handle operations "+", "-", "*", "/"
+  if (["+", "-", "*", "/"].indexOf(value) > -1) {
+    if (isNumber(lastChar) || lastChar === ")") return old + value;
+    if (lastChar === ".") return old + "0" + value;
+  }
+
+  // handle numbers
+  if (isNumber(value)) {
+    // add multiplication if after closing
+    if (lastChar === ")") return old + "*" + value;
+
+    return old + value;
+  }
+
+  if (value === "+/-") {
+    if (isNumber(lastChar)) {
+      const lastNum = getLastFullNumber(old);
+      const lastNumStr = lastNum.toString();
+      const newNumStr =
+        lastNum > 0 ? `(-${lastNumStr}` : lastNumStr.slice(2, -1);
+
+      return old.slice(0, old.lastIndexOf(lastNumStr)) + newNumStr;
+    }
+  }
+
+  // TODO handle percent %
+
+  return old;
+};
 
 export default function NormalCalculator() {
   const [input, setInput] = useState<string>("");
   const [result, setResult] = useState<number | string>("");
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const [history, setHistory] = useState<[string, string, number][]>([]);
 
   const handleClick = (value: string) => {
-    setInput(input + value);
+    // handle clear
+    if (value === "C") return handleClear();
+
+    // handle equal
+    // TODO add history on equal click
+    if (value === "=") {
+      return handleCalculate(input, true);
+    }
+
+    const newInput = handleInput(input, value);
+    setInput(newInput);
+    handleCalculate(newInput);
   };
 
   const handleClear = () => {
@@ -14,25 +144,48 @@ export default function NormalCalculator() {
     setResult("");
   };
 
-  const handleCalculate = () => {
+  const handleCalculate = (value: string, isForced = false) => {
     try {
       // eslint-disable-next-line no-eval
-      const evalResult = eval(input);
+      const evalResult = eval(value.length ? value : "0");
       setResult(evalResult);
+
+      if (isForced) {
+        setHistory((p) => [[uuidv4(), value, evalResult], ...p]);
+      }
+      // if (!isNaN(evalResult)) setInput(`${evalResult}`);
     } catch {
       setResult("Error");
     }
   };
 
+  const handleErase = () => {
+    setInput((p) => p.substring(0, p.length - 1));
+  };
+
   return (
-    <Box p={2} sx={{ width: "min(100% , 400px)", margin: "0 auto" }}>
+    <Box
+      p={2}
+      sx={{
+        width: "min(100% , 400px)",
+        margin: "0 auto",
+        display: "flex",
+        flexDirection: "column",
+        maxHeight: "100%",
+        overflow: "hidden",
+      }}
+    >
       <TextField
         label="Input"
         variant="outlined"
         fullWidth
         value={input}
+        onChange={() => {
+          //do nothing
+        }}
         disabled
         sx={{ mb: 2 }}
+        ref={inputRef}
       />
       <TextField
         label="Result"
@@ -42,12 +195,28 @@ export default function NormalCalculator() {
         disabled
         sx={{ mb: 2 }}
       />
+
+      <Box
+        sx={{
+          marginBottom: 1,
+          paddingBottom: 1,
+          borderBottom: "1px solid black",
+        }}
+      >
+        <Button variant="contained" onClick={handleErase}>
+          erase
+        </Button>
+      </Box>
       <Grid container spacing={1}>
         {[
+          "C",
+          "()",
+          "%",
+          "/",
           "7",
           "8",
           "9",
-          "+",
+          "*",
           "4",
           "5",
           "6",
@@ -55,43 +224,47 @@ export default function NormalCalculator() {
           "1",
           "2",
           "3",
-          "*",
+          "+",
           "+/-",
           "0",
           ".",
-          "/",
+          "=",
         ].map((value) => (
           <Grid item xs={3} key={value}>
             <Button
               variant="contained"
               fullWidth
-              onClick={() => handleClick(value)}
+              onClick={() => {
+                handleClick(value);
+              }}
+              color={
+                value === "C"
+                  ? "error"
+                  : value === "="
+                  ? "success"
+                  : ["()", "%", "/", "*", "-", "+"].indexOf(value) > -1
+                  ? "secondary"
+                  : "primary"
+              }
             >
               {value}
             </Button>
           </Grid>
         ))}
-        <Grid item xs={6}>
-          <Button
-            variant="contained"
-            color="error"
-            fullWidth
-            onClick={handleClear}
-          >
-            Clear
-          </Button>
-        </Grid>
-        <Grid item xs={6}>
-          <Button
-            variant="contained"
-            color="primary"
-            fullWidth
-            onClick={handleCalculate}
-          >
-            =
-          </Button>
-        </Grid>
       </Grid>
+
+      <Typography mt={3}>History</Typography>
+      <Box sx={{ flex: 1, overflow: "auto" }}>
+        <List component="nav" disablePadding dense>
+          {history.map((h) => (
+            <ListItem key={h[0]}>
+              <ListItemButton onClick={() => setInput(h[1])} dense>
+                <ListItemText primary={h[1]} secondary={`=${h[2]}`} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Box>
     </Box>
   );
 }
